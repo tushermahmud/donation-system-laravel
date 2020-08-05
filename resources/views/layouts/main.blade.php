553d23e1367f01411d98577bd33f3c3c71a9703c
sazzadmahmud1417027 <tusher@appnap.io>
2020-08-05 21:28:27 +0600

    database setup

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,11 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
     <!-- FontAwesome CSS -->
-    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!-- ElegantFonts CSS -->
     <link rel="stylesheet" href="{{asset('css/elegant-fonts.css')}}">
 
@@ -228,8 +226,9 @@
         </div><!-- .footer-bar -->
     </footer><!-- .site-footer -->
 
-    <script type='text/javascript' src="{{asset('js/jquery.js')}}"></script>
-    <script type='text/javascript' src="{{asset('js/jquery.collapsible.min.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>    <script type='text/javascript' src="{{asset('js/jquery.collapsible.min.js')}}"></script>
     <script type='text/javascript' src="{{asset('js/swiper.min.js')}}"></script>
     <script type='text/javascript' src="{{asset('js/jquery.countdown.min.js')}}"></script>
     <script type='text/javascript' src="{{asset('js/circle-progress.min.js')}}"></script>
